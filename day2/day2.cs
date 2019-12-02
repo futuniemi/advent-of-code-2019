b0a9093ea4340fb52ea48b9a9c07384ef09c1f6c
Minna Niemi <minna.niemi@futurice.com>
2019-12-02 21:22:53 +0000

    Add solution for day2 part 2

--- day2/day2.cs
+++ day2/day2.cs
@@ -5,30 +5,45 @@ using System.Linq;
 
 namespace advent_of_code_2019.day2
 {
-
     public static class Day2
     {
-
         private static int pointer = 0;
         private static List<int> program;
+        private static List<int> cleanNumbers = null;
 
         public static void Run()
         {
-            program = GetNumbers();
             try
             {
-                IterateProgram();
+                var nouns = Enumerable.Range(0, 100);
+                var verbs = Enumerable.Range(0, 100);
+                foreach (int noun in nouns)
+                {
+                    foreach (int verb in verbs)
+                    {
+                        pointer = 0;
+                        program = new List<int>(GetNumbers());
+                        program[1] = noun;
+                        program[2] = verb;
+                        IterateProgram();
+                        if (program[0] == 19690720)
+                        {
+                            throw new Exception($"Found it! Noun: {noun}, Verb: {verb}");
+                        }
+                    }
+                }
             }
-            catch
+            catch (Exception e)
             {
-                Console.WriteLine(String.Join(", ", program));
+                Console.WriteLine(e.Message);
             }
         }
 
         private static void IterateProgram()
         {
+            var stop = false;
             var maxLength = program.Count();
-            while (pointer < maxLength)
+            while (pointer < maxLength && !stop)
             {
                 var opCode = program[pointer];
                 var param1 = program[pointer + 1];
@@ -43,12 +58,14 @@ namespace advent_of_code_2019.day2
                         Multiply(param1, param2, param3);
                         break;
                     case 99:
-                        Terminate();
+                        stop = true;
                         break;
                     default:
                         throw new Exception("Command not 1, 2 or 99");
                 }
                 pointer += 4;
+                if (stop)
+                    break;
             }
         }
 
@@ -62,18 +79,17 @@ namespace advent_of_code_2019.day2
             program[param3] = program[param1] * program[param2];
         }
 
-        private static void Terminate()
-        {
-            throw new Exception("Program terminated");
-        }
-
         private static List<int> GetNumbers()
         {
-            var filePath = Path.Combine(
-                new string[] { Directory.GetCurrentDirectory(), "day2", "day2-input" }
-            );
-            var content = File.ReadAllText(filePath);
-            return content.Split(",").ToList().Select(item => int.Parse(item)).ToList();
+            if (cleanNumbers == null)
+            {
+                var filePath = Path.Combine(
+                    new string[] { Directory.GetCurrentDirectory(), "day2", "day2-input" }
+                );
+                var content = File.ReadAllText(filePath);
+                cleanNumbers = content.Split(",").ToList().Select(item => int.Parse(item)).ToList();
+            }
+            return cleanNumbers;
         }
     }
 }
