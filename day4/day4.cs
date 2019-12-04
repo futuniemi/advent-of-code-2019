b4567ad4c6a1e323c54794f66ee44f4fcf4ff73b
Minna Niemi <minna.niemi@futurice.com>
2019-12-04 20:46:13 +0000

    Add solution for day 4 part 2

--- day4/day4.cs
+++ day4/day4.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace advent_of_code_2019.day4
@@ -7,11 +8,22 @@ namespace advent_of_code_2019.day4
     {
         public static void Run()
         {
-            var possiblePasswords = Enumerable.Range(134792, 675810 - 134792)
-                .Select(x => x.ToString().ToCharArray())
-                .Where(x => x.SequenceEqual(x.OrderBy(y => y)))
-                .Where(x => Enumerable.Range(0, 5).Any(z => x[z] == x[z + 1]));
-            Console.WriteLine(possiblePasswords.Count());
+            Console.WriteLine("Answer to Part 1: " + CountPart1());
+            Console.WriteLine("Answer to Part 2: " + CountPart2());
         }
+
+        private static int CountPart1() => GetBase()
+            .Where(x => Enumerable.Range(0, 5).Any(z => x[z] == x[z + 1]))
+            .Count();
+
+        private static int CountPart2() => GetBase()
+            .Where(x => Enumerable.Range(0, 5).Any(z => x[z] == x[z + 1]
+                        && x.Where(n => n == x[z]).Count() == 2))
+            .Count();
+
+        private static IEnumerable<char[]> GetBase() =>
+            Enumerable.Range(134792, 675810 - 134792)
+            .Select(x => x.ToString().ToCharArray())
+            .Where(x => x.SequenceEqual(x.OrderBy(y => y)));
     }
 }
